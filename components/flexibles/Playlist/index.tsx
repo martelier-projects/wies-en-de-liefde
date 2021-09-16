724b383c117a6b16d0df61683546e5afde7807fe
Martijn Nieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2021-09-16 20:46:31 +0200

    Fix playlist

--- components/flexibles/Playlist/index.tsx
+++ components/flexibles/Playlist/index.tsx
@@ -56,6 +56,7 @@ export default function PlayList({ title }: PlaylistInterface) {
             ))}
           </ol>
         </div>
+        <div className={styles['playlist__background']} />
       </div>
     </div>
   )
